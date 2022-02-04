e078adb11f7cf25e2797df50fc46c8143f1b4e1c
Aman Garg (LHIND) <garg@indivumed.com>
2022-02-04 16:08:13 +0100

    sns topic again updated

--- terraform-manifests/c6-01-sns.tf
+++ terraform-manifests/c6-01-sns.tf
@@ -3,19 +3,20 @@
 ## Due to that create SNS Topic with unique name
 
 ## SNS - Topic
-resource "aws_sns_topic" "myasg_sns_topic" {
-  name = "myasg-sns-topic-${random_pet.this.id}"
+resource "aws_sns_topic" "myec2_sns_topic" {
+  name = "${local.environment}-sns-topic-${random_pet.this.id}"
 }
 
 ## SNS - Subscription
-resource "aws_sns_topic_subscription" "myasg_sns_topic_subscription" {
-  topic_arn = aws_sns_topic.myasg_sns_topic.arn
+resource "aws_sns_topic_subscription" "myec2_sns_topic_subscription" {
+  topic_arn = aws_sns_topic.myec2_sns_topic.arn
   protocol  = "email"
-  endpoint  = "stacksimplify@gmail.com"
+  endpoint  = "manipal.amangarg@gmail.com"
 }
 
+/*
 ## Create Autoscaling Notification Resource
-resource "aws_autoscaling_notification" "myasg_notifications" {
+resource "aws_autoscaling_notification" "myec2_notifications" {
   group_names = [aws_autoscaling_group.my_asg.id]
   notifications = [
     "autoscaling:EC2_INSTANCE_LAUNCH",
@@ -25,3 +26,4 @@ resource "aws_autoscaling_notification" "myasg_notifications" {
   ]
   topic_arn = aws_sns_topic.myasg_sns_topic.arn
 }
+*/
